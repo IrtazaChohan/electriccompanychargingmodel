c0644ca70b9d1f290d2b4d99456ccd1e1f96c459
Irtaza Chohan <irtazachohan@gmail.com>
2021-09-27 12:52:10 +0100

    Add project files.

---
+++ electriccompanychargingmodel/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace electriccompanychargingmodel
+{
+    partial class FrmElectrcCharge
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChargeBox = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtChargeBox
+            // 
+            this.txtChargeBox.Location = new System.Drawing.Point(191, 56);
+            this.txtChargeBox.Name = "txtChargeBox";
+            this.txtChargeBox.Size = new System.Drawing.Size(100, 23);
+            this.txtChargeBox.TabIndex = 0;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(156, 82);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(175, 15);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Enter amount of Electricity used";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(194, 127);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(96, 27);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(191, 175);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Visible = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(194, 205);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(97, 26);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // FrmElectrcCharge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 344);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtChargeBox);
+            this.Name = "FrmElectrcCharge";
+            this.Text = "Electric Charge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtChargeBox;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ electriccompanychargingmodel/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace electriccompanychargingmodel
+{
+    public partial class FrmElectrcCharge : Form
+    {
+        public FrmElectrcCharge()
+        {
+            InitializeComponent();
+        }
+        //An electric company measures the amount of electricity its customer use in kilowatt hours(kwh) and charges them
+        //according to the following schedule:
+        //first 12 kwh or less $2.80
+        //next 78 kwh 8 cents each kwh
+        //excess above 90 kwh 6 cents each kwh
+        //Note that the minimum bill is $2.80. Write a program to calculate a customers bill.
+
+        int electric = 0;
+        double charge = 0;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            electric = int.Parse(txtChargeBox.Text);
+            if(electric <= 12)
+            {
+                charge = 2.80;
+                string formattedMoneyValue = String.Format("{0:C2}", charge);
+                lblResult.Text = "Your charge is " + formattedMoneyValue;
+                lblResult.Visible = true;
+            }
+            else if(electric >12 && electric <= 90)
+            {
+
+                charge = ((electric - 12) * 0.08) + 2.80;
+                string formattedMoneyValue = String.Format("{0:C2}", charge);
+                lblResult.Text = "Your charge is " + formattedMoneyValue;
+                lblResult.Visible = true;
+            }
+            else if(electric > 90)
+            {
+                charge = (78 * 0.08) + ((electric - 90) * 0.06) + 2.80;
+                string formattedMoneyValue = String.Format("{0:C2}", charge);
+                lblResult.Text = "Your charge is " + formattedMoneyValue;
+                lblResult.Visible = true;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtChargeBox.Text = "";
+        }
+    }
+}
